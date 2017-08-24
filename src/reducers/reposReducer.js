422bc553d96b622bfc9af61e87c3a80ecd99a47a
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-25 04:25:25 +0530

    Added a reposReducer

--- src/reducers/reposReducer.js
+++ src/reducers/reposReducer.js
@@ -2,8 +2,9 @@ import { LOAD_REPOS_SUCCESS } from '../actions/types';
 
 export default function reposReducer(state = [], action) {
   switch (action.type) {
-    case LOAD_REPOS_SUCCESS:
+    case LOAD_REPOS_SUCCESS: {
       return action.repos;
+    }
 
     default:
       return state;
