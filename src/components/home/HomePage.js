9f0510874e7b47a99e2836b596d7e41d21748a8f
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-25 17:31:23 +0530

    Added the Searchbar to homepage and helper functions and also called the mapStateToProps and mapDispatchToProps

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as userActions from '../../actions/userAction';
 
 import Searchbar from './Searchbar';
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: ''
+    };
+    this.updateSearch = this.updateSearch.bind(this);
+    this.saveSearch = this.saveSearch.bind(this);
+  }
+
+  updateSearch(e) {
+    let user = e.target.value;
+    return this.setState({ user: user });
+  }
+
+  saveSearch(e) {
+    e.preventDefault();
+    this.props.actions.loadUser(this.state.user);
+  }
+
   render() {
     return (
       <div className="container">
@@ -16,4 +38,17 @@ class HomePage extends Component {
   }
 }
 
-export default HomePage;
+function mapStateToProps(state) {
+  return {
+    repos: state.repos,
+    user: state.user
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(userActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
